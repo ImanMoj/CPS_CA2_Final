73984a91ff70a585e8bfcb5d564cb2f17468cc11
iman moj <iman.mojtahed@gmail.com>
2019-02-22 12:41:19 +0330

    temp and distance sensors work correctly and are concatinated properly but are not sent.

--- Sensor_Board/SensorBoard/src/main.cpp
+++ Sensor_Board/SensorBoard/src/main.cpp
@@ -1,19 +1,30 @@
 #include <Arduino.h>
 #include <Wire.h>
-// #include <String.h>
+#include <String.h>
 #include <LM35.h>
+#include <HCSR04.h>
 
+
+float temperature;
+const int trigPin = 12;
+const int echoPin = 13;
+long duration;
+int distance;
+String message;
+String str_temperature ;
+String str_distance ;
 LM35 temp_sensor(A1);
-int value;
+HCSR04 hcsr04(trigPin, echoPin, 20, 4000);
 
-void sendTemperature(){
-  
-  value=temp_sensor.cel();
-  char b [4];
-  itoa(value, b, 10);
-  Serial.write(b , 8);
-  delay(400);
-}
+String concatinate(float temperature , int distance){
+   
+   str_temperature = String(temperature);
+   str_distance = String(distance);
+   str_temperature += "$";
+   str_temperature += str_distance;
+   return str_temperature ;
+   
+ }
 
 void setup()
 {
@@ -22,6 +33,10 @@ void setup()
 
 void loop()
 {
-  sendTemperature();
+  temperature = temp_sensor.cel();
+  distance = hcsr04.distanceInMillimeters();
+  message = concatinate(temperature , distance);
+  Serial.println(message);
+  delay(500);
 }
 
